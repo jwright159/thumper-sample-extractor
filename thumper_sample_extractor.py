a94355b5ced9368106c240c3e4ec0327c618605a
Jade Wright <iamjwright159@gmail.com>
2023-04-08 17:21:09 -0400

    Add basis for extracting arbitrary filetypes

--- thumper_sample_extractor.py
+++ thumper_sample_extractor.py
@@ -1,11 +1,17 @@
+"""Documentation: https://rainbowunicorn7297.github.io/"""
+
 from io import BufferedReader
 from os import listdir, makedirs
 from fsb5 import FSB5
-from typing import Set, Tuple
+from typing import Callable, Set, Tuple
 from os.path import join, dirname, exists
 from numpy import uint32
 
 def hash32(f: str) -> str:
+	"""A magic number is a 32-bit hash value produced by applying a hash function on a string or a byte array.
+	Hash tables are maintained in the game executable mapping hash values to objects or string literals like game file paths,
+	object types and parameter names. This is probably done to avoid expensive string operations such as comparisons and creating new copies,
+	and to allow fast lookup of objects using the hash value as the ID."""
 	f = 'A' + f
 	h = uint32(0x811c9dc5)
 	for c in f:
@@ -15,88 +21,169 @@ def hash32(f: str) -> str:
 	h = uint32(uint32(h ^ uint32(h >> 0x11)) * 0x21)
 	return hex(h)[2:]
 
-sample_set: Set[str] = set()
-query = b'samples'
-ext_query = b'.wav'
-def get_sample_list(file_name: str, pc: BufferedReader):
-	pc.seek(0)
+magic_header_descriptors: dict[bytes, str] = {
+	bytes.fromhex('4314a51b'): 'GFX',
+	bytes.fromhex('484595b0'): 'Sequin',
+	bytes.fromhex('19621c9d'): 'Obj',
+	bytes.fromhex('9e4d370b'): 'Level',
+	bytes.fromhex('4f6274e6'): 'Avatar',
+	b'FSB5': 'Audio',
+	b'DDS ': 'Texture',
+}
 
+def list_file_headers(file_name: str, pc: BufferedReader):
+	pc.seek(0)
+	pc_type = int.from_bytes(pc.read(4), 'little')
+	pc_magic_header = pc.read(4)
+	print(file_name, pc_type, magic_header_descriptors[pc_magic_header] if pc_magic_header in magic_header_descriptors else pc_magic_header)
+
+def find_file_references(file_name: str, pc: BufferedReader, start_query: str, end_query: str):
+	"""Takes a file and finds all references to other filepaths that start with `query` and end in `ext_query`"""
+	start_query_bytes = start_query.encode()
+	end_query_bytes = end_query.encode()
+
+	pc.seek(0)
+	pc_type = int.from_bytes(pc.read(4), 'little')
+	pc_magic_header = pc.read(4)
+
+	pc.seek(0)
 	data = pc.read()
 	barr = bytearray(data)
-	path_index = barr.find(query)
+	path_index = barr.find(start_query_bytes)
 	while path_index >= 0:
-		ext_index = barr.index(ext_query, path_index)
-		file_type = int.from_bytes(data[:4], 'little')
-		obj_type = data[4:8].hex()
-		obj_type = \
-			'gfx' if obj_type == '4314a51b' else \
-			'seq' if obj_type == '484595b0' else \
-			'obj' if obj_type == '19621c9d' else \
-			'lev' if obj_type == '9e4d370b' else \
-			'ava' if obj_type == '4f6274e6' else \
-			obj_type
-		sample_path = data[path_index:ext_index+4].decode()
+		ext_index = barr.index(end_query_bytes, path_index)
+		target_path = data[path_index:ext_index+4].decode()
 
-		print(file_name, file_type, obj_type, hex(path_index), sample_path)
-		sample_set.add(sample_path)
+		print(file_name, pc_type, pc_magic_header, magic_header_descriptors[pc_magic_header] if pc_magic_header in magic_header_descriptors else pc_magic_header, hex(path_index), target_path)
+		yield target_path
 
-		path_index = barr.find(query, ext_index)
+		path_index = barr.find(start_query_bytes, ext_index)
 
-def get_sample(cache_path: str, pc: BufferedReader, save_path: str = ''):
+Extractor = Callable[[BufferedReader, str, str | None], None]
+
+def extract_mesh(pc: BufferedReader, cache_path: str, save_path: str | None):
+	if save_path is None:
+		save_path = join('samples', 'models', f"{cache_path}.x")
+	print(save_path)
+	makedirs(dirname(save_path), exist_ok=True)
+	with open(save_path, 'wb') as mesh_file:
+		mesh_file.write(pc.read())
+
+def extract_audio(pc: BufferedReader, cache_path: str, save_path: str | None):
+	fsb = FSB5(pc.read())
+	#print(f, fsb.header)
+	
+	ext = fsb.get_sample_extension()
+
+	for sample in fsb.samples:
+		if save_path is not None:
+			save_path = save_path[:save_path.rindex('.')] + '.' + ext # it's ogg not wav
+		else:
+			save_path = join('samples', f"{sample.name}.{ext}")
+		#print(cache_path, save_path)
+
+		makedirs(dirname(save_path), exist_ok=True)
+
+		with open(save_path, 'wb') as sample_file:
+			rebuilt_sample = fsb.rebuild_sample(sample)
+			sample_file.write(rebuilt_sample)
+
+file_type_extractors: dict[int, dict[bytes, Extractor | None] | Extractor | None] = {
+	0: None,	# Scoring file. Defines the scoring rules, such as how many points are awarded for each type of action and no miss and no damage bonuses.
+	4: None,	# Config files
+	5: None,	# A list of UI menus, such as the main menu (Credits, Level Select, Leaderboards, Options) and in-game pause screen (Restart Checkpoint, Exit)
+	6: extract_mesh,	# 3D mesh (stripped-down .x, DirectX file format) files and localization files
+	8: { # Game object library (.objlib, custom format) files
+		bytes.fromhex('4314a51b'): None,	# GFX library. Most .objlib files that do not fit into the other types are in this category, including .objlib files that contain UI objects, bosses, VFX, skyboxes, interactive visual objects, and decorative visual objects with simple animations.
+		bytes.fromhex('484595b0'): None,	# Sequin library. These are .objlib files that contain decorative visual objects with complex animations that require more sequencing control.
+		bytes.fromhex('19621c9d'): None,	# Obj library. These are .objlib files that control the audio aspect of the game, including channels and SFX, and VR playspace scale
+		bytes.fromhex('9e4d370b'): None,	# Level library. These are .objlib files that control the sequence and timing of objects appearing in a level.
+		bytes.fromhex('4f6274e6'): None,	# Avatar library. This is the .objlib file that control the appearance of the beetle and its reactions to input and obstacles.
+	},
+	9: None,	# Credits files and level config files
+	12: None,	# Texture (.dds, DirectDraw Surface) files
+	13: {
+		b'FSB5': extract_audio,	# Audio sample (FSB5, FMOD Sample Bank) files
+		b'DDS ': None,	# Texture (.dds, DirectDraw Surface) files
+	},
+	14: None,	# A list of levels (Level 1 – Level 9)
+	28: None,	# Shader files
+	93: None,	# Unknown
+}
+"""https://rainbowunicorn7297.github.io/#Overall%20Structure%20of%20Game%20Files"""
+
+def extract_file(cache_path: str, pc: BufferedReader, file_type: int, magic_header: bytes | None = None, save_path: str | None = None):
 	pc.seek(0)
+	pc_file_type = int.from_bytes(pc.read(4), 'little')
+	pc_magic_header = pc.read(4)
+	if pc_file_type == file_type and (pc_magic_header == magic_header if magic_header else True):
+		pc.seek(4) # Actual data starts here (includes magic header)
 
-	# we want type 13, or "Audio sample (FSB5, FMOD Sample Bank) files and texture (.dds, DirectDraw Surface) files"
-	pc_type = int.from_bytes(pc.read(4), 'little')
-	pc_magic_header = pc.read(4) # Gonna be "FSB5" or "DDS "
-	if pc_type == 13 and pc_magic_header == b'FSB5':
-
-		pc.seek(4) # Actual data starts here
-		
-		fsb = FSB5(pc.read())
-		#print(f, fsb.header)
-		
-		ext = fsb.get_sample_extension()
-
-		for sample in fsb.samples:
-			if save_path:
-				save_path = save_path[:save_path.rindex('.')] + '.' + ext # it's ogg not wav
+		if file_type not in file_type_extractors:
+			print(f'File type {file_type} not understood')
+		else:
+			extractor_group = file_type_extractors[file_type]
+			if extractor_group is None:
+				print(f'No extractor created for file type {file_type}')
+			elif isinstance(extractor_group, dict):
+				if magic_header is None:
+					print(f'No magic header supplied for file type {file_type} where one is needed')
+				elif magic_header not in extractor_group:
+					print(f'File type {file_type} with magic header {magic_header} not understood')
+				else:
+					extractor = extractor_group[magic_header]
+					if extractor is None:
+						print(f'No extractor created for file type {file_type} and magic header {magic_header}')
+					else:
+						extractor(pc, cache_path, save_path)
 			else:
-				save_path = join('samples', f"{sample.name}.{ext}")
-			#print(cache_path, save_path)
+				extractor = extractor_group
+				extractor(pc, cache_path, save_path)
 
-			makedirs(dirname(save_path), exist_ok=True)
-
-			with open(save_path, 'wb') as sample_file:
-				rebuilt_sample = fsb.rebuild_sample(sample)
-				sample_file.write(rebuilt_sample)
-
-
-if __name__ == '__main__':
+def find_all_references_and_extract(description: str, start_query: str, end_query: str, file_type: int, magic_header: bytes | None = None):
+	target_paths: Set[str] = set()
 
 	files = [f for f in listdir() if f[-3:] == '.pc']
 	for f in files:
 		with open(f, 'rb') as pc:
-			get_sample_list(f, pc)
-			#get_sample(f, pc)
+			target_paths.update(find_file_references(f, pc, start_query, end_query))
 
-	if sample_set:
-		with open('sample_list.txt', 'w') as sample_txt:
-			sample_txt.write('\n'.join(sorted(list(sample_set), key=str.casefold)))
-		
-		bad_caches: Set[Tuple[str, str]] = set()
-		for sample_path in sample_set:
-			pc_path = hash32(sample_path) + '.pc'
-			print(pc_path, sample_path, end='')
+	with open(f'{description} list.txt', 'w') as list_txt:
+		list_txt.write('\n'.join(sorted(list(target_paths), key=str.casefold)))
+	
+	missing_caches: Set[Tuple[str, str]] = set()
+	for target_path in target_paths:
+		pc_path = hash32(target_path) + '.pc'
 
-			if exists(pc_path):
-				print()
-				with open(pc_path, 'rb') as pc:
-					get_sample(pc_path, pc, sample_path)
-			
-			else:
-				print(" not found!")
-				bad_caches.add((pc_path, sample_path))
+		if exists(pc_path):
+			print(pc_path, target_path)
+			with open(pc_path, 'rb') as pc:
+				extract_file(pc_path, pc, file_type, magic_header, target_path)
 		
-		if bad_caches:
-			with open('missing_caches.txt', 'w') as bad_cache_txt:
-				bad_cache_txt.write("These cache files were not found!\n" + '\n'.join([f"{x[0]} {x[1]}" for x in sorted(list(bad_caches), key=lambda x: str.casefold(x[1]))]))
+		else:
+			print(pc_path, target_path, 'not found!')
+			missing_caches.add((pc_path, target_path))
+	
+	if missing_caches:
+		with open(f'missing {description} caches.txt', 'w') as bad_cache_txt:
+			bad_cache_txt.write('These cache files were not found!\n' + '\n'.join([f'{x[0]} {x[1]}' for x in sorted(list(missing_caches), key=lambda x: str.casefold(x[1]))]))
+
+def attempt_extract_all(file_type: int, magic_header: bytes | None = None):
+	files = [f for f in listdir() if f[-3:] == '.pc']
+	for f in files:
+		with open(f, 'rb') as pc:
+			extract_file(f, pc, file_type, magic_header)
+
+def list_all_file_headers():
+	files = [f for f in listdir() if f[-3:] == '.pc']
+	for f in files:
+		with open(f, 'rb') as pc:
+			list_file_headers(f, pc)
+
+def main():
+	find_all_references_and_extract('audio sample', 'samples', '.wav', 13, b'FSB5')
+	#attempt_extract_all(6, 'xof ') #bytes.fromhex('01000000'))
+	#list_all_file_headers()
+
+if __name__ == '__main__':
+	main()
